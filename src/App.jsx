d8faa3875e325993def3175f224414cf17409379
KAUSAR-BD <mdkausarsah2000@gmail.com>
2024-12-06 19:42:07 +0600

    Let's start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-slate-900 text-4xl text-red-400 mt-10 text-center font-extrabold">Project Real State One</h1>
+    </div>
+  );
+};
+
+export default App;
